4bd551200f0703859492786bbf5a415cdef52981
techiesabina <techiesabina@users.noreply.github.com>
2019-06-26 14:35:31 -0400

    First commit to test.java

---
+++ test.java
@@ -0,0 +1,18 @@
+public class Test
+{
+	private String name;
+	private int age;
+
+	public Test(String name, int age)
+	{
+		this.name = name;
+		this.age = age;		
+	}
+	
+	public static void main(String[] args)
+	{	
+		Test new = new Test("Bob Marley", 200);
+		System.out.print.ln("Hi! My name is " + this.name +
+		 ", and I'm " + age + " years old!" );
+	}
+}
