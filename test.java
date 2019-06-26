7cf8beef9bbfa71abb4177e2870685c725318820
techiesabina <techiesabina@users.noreply.github.com>
2019-06-26 14:40:25 -0400

    Add toString and edit main method for test.java

--- test.java
+++ test.java
@@ -8,11 +8,16 @@ public class Test
 		this.name = name;
 		this.age = age;		
 	}
+
+	public static String toString()
+	{
+		return "Hi! My name is " + this.name +
+                 ", and I'm " + age + " years old! #^3^# " );
+	}	
 	
 	public static void main(String[] args)
 	{	
 		Test new = new Test("Bob Marley", 200);
-		System.out.print.ln("Hi! My name is " + this.name +
-		 ", and I'm " + age + " years old!" );
+		System.out.print.ln(new);
 	}
 }
